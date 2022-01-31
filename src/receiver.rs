b2bd0e20a11c78bba5c2af40a381959625b1e8a4
Keijiro Takahashi <keijiro@gmail.com>
2022-01-31 22:36:39 +0900

    Simpler form

--- src/receiver.rs
+++ src/receiver.rs
@@ -1,7 +1,7 @@
 fn main() {
     let ctx = zmq::Context::new();
     let socket = ctx.socket(zmq::SUB).unwrap();
-    socket.set_subscribe(b"trigger").unwrap();
+    socket.set_subscribe(b"").unwrap();
     socket.bind("tcp://*:5556").unwrap();
     loop {
         let data = socket.recv_string(0).unwrap().unwrap();
--- src/sender.rs
+++ src/sender.rs
@@ -2,9 +2,9 @@ fn main() {
     let ctx = zmq::Context::new();
     let socket = ctx.socket(zmq::PUB).unwrap();
     socket.connect("tcp://localhost:5556").unwrap();
-    let text = format!("Hello [{}]", std::process::id()).to_string();
+    let text = format!("Source: [{}]", std::process::id()).to_string();
     loop {
-        socket.send_multipart(&["trigger", &text], 0).unwrap();
-        std::thread::sleep(std::time::Duration::from_millis(500));
+        socket.send(&text, 0).unwrap();
+        std::thread::sleep(std::time::Duration::from_millis(200));
     }
 }
