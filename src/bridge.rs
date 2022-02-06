8cbf3ace020a71f9c693015f332365ba7123c000
Keijiro Takahashi <keijiro@gmail.com>
2022-02-06 18:16:15 +0900

    Support command line argument

--- src/bridge.rs
+++ src/bridge.rs
@@ -1,6 +1,11 @@
+use std::{str, env};
+use std::net::{SocketAddr, ToSocketAddrs, UdpSocket};
 use rosc::{OscMessage, OscPacket, OscType};
-use std::net::UdpSocket;
-use std::str;
+
+fn get_destination_addr() -> SocketAddr {
+    let addr_str = env::args().nth(1).unwrap_or("127.0.0.1:9000".to_string());
+    addr_str.to_socket_addrs().unwrap().next().unwrap()
+}
 
 fn main() {
     let recv_sock = zmq::Context::new().socket(zmq::SUB).unwrap();
@@ -8,6 +13,9 @@ fn main() {
     recv_sock.bind("tcp://*:53311").unwrap();
 
     let send_sock = UdpSocket::bind("0.0.0.0:0").unwrap();
+    let send_addr = get_destination_addr();
+
+    println!("Started routing messages to {}", send_addr);
 
     loop {
         let recv_msg = recv_sock.recv_multipart(0).unwrap();
@@ -20,6 +28,6 @@ fn main() {
         let osc_pak = OscPacket::Message(osc_msg);
         let send_msg = rosc::encoder::encode(&osc_pak).unwrap();
 
-        send_sock.send_to(&send_msg, "127.0.0.1:9000").unwrap();
+        send_sock.send_to(&send_msg, send_addr).unwrap();
     }
 }
